a04e5bb20cb9e2b2eeff1b06be524bbc9165eef5
dilriniwg <dilriniwg.21@uom.lk>
2024-06-14 04:11:39 +0530

    Update Newapp.py

--- Newapp.py
+++ Newapp.py
@@ -96,14 +96,6 @@ col1, col2 = st.columns([1, 3])
 with col1:
     st.image("images/fixed_ends.jpg", caption="Fixed end beam", width=300)
 
-with col2:
-    st.markdown("""
-    <div style="background-color:lightblue;padding:10px;border-radius:10px;">
-    This application calculates the deflection and rotation of a beam that is supported by two fixed ends based on its cross-sectional shape, load type, 
-    and other parameters. Please select the appropriate options and enter the required values to see the results.
-    </div>
-    """, unsafe_allow_html=True)
-
 # Apply CSS styling to center align the image
 st.markdown(
     """
@@ -116,6 +108,14 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
+ st.markdown("""
+    <div style="background-color:lightblue;padding:10px;border-radius:10px;">
+    This application calculates the deflection and rotation of a beam that is supported by two fixed ends based on its cross-sectional shape, load type, 
+    and other parameters. Please select the appropriate options and enter the required values to see the results.
+    </div>
+    """, unsafe_allow_html=True)
+
 shape = st.selectbox("Select the cross-sectional shape of the beam:", ["Circle", "Square", "Rectangle", "I-Beam"])
 
 if shape == 'Circle':

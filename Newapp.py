e3cb4c45857a9511ee3bb55450d35fb5cbec3e2b
dilriniwg <dilriniwg.21@uom.lk>
2024-06-14 04:07:17 +0530

    Update Newapp.py

--- Newapp.py
+++ Newapp.py
@@ -99,9 +99,7 @@ and other parameters. Please select the appropriate options and enter the requir
 </div>
 """, unsafe_allow_html=True)
 
-col1, col2 = st.columns([1, 4])
-with col1:
-    st.image("images/fixed_ends.jpg", caption="Fixed end beam", width=300)
+st.image("images/fixed_ends.jpg", caption="Fixed end beam", width=200)
 
 shape = st.selectbox("Select the cross-sectional shape of the beam:", ["Circle", "Square", "Rectangle", "I-Beam"])
 
